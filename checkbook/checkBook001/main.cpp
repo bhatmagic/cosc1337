a8f265a78f2a66be3d4b349cecac2acaf8bf30ad
bhatmagic <bhatmagician@gmail.com>
2019-02-24 18:17:30 -0600

    updated fns

--- checkbook/checkBook001/main.cpp
+++ checkbook/checkBook001/main.cpp
@@ -12,6 +12,10 @@
 bool endProgram(double&, double&);
 bool processCheck(double&, double&);
 bool processDeposit(double&, double&);
+void displayMenu(void);
+void displayTitle(void);
+void processTransactions(double&, double&);
+double begBalance();
 
 // Global variables
 double SERV_CHARGE = 0.35;
@@ -22,36 +26,16 @@ double SERV_CHARGE = 0.35;
 int main () {
     
     // Declare variables
-    char command;
-    double balance, 
-           totalCharges = 0.00; 
-    std::string prgmTitle = "Checkbook Balancing Program"; 
+    double balance,
+          totalCharges = 0.00;
 
     // Format displayed numbers to two decimal places
     std::cout << std::fixed << std::showpoint << std::setprecision(2); 
 
     // Display title and menu
-    std::cout << prgmTitle << std::endl;
-    std::cout << "\nEnter the beginning balance: ";
-    std::cin  >> balance;
-
-    std::cout << "\nCommands:\n";
-    std::cout << "C - process a check\n";
-    std::cout << "D - process a deposit\n";    
-    std::cout << "E - end the programn";    
-        
-    do {
-        std::cout << "\n\nEnter transaction type: ";
-        std::cin  >> command;
-        command = tolower(command);
-
-        switch(command) {
-            case 'c': processCheck(balance, totalCharges); break;
-            case 'd': processDeposit(balance, totalCharges); break;
-            case 'e': break;
-            default : std::cout << "\nNot a valid command.\n"; break;
-        } 
-    } while (command != 'e');
+    balance = begBalance();
+    displayMenu();
+    processTransactions(balance, totalCharges);
 
     endProgram(balance, totalCharges); 
     std::cout << std::endl;
@@ -124,4 +108,52 @@ bool processDeposit(double &balance, double &totalCharges) {
     }
 
     return 0;
+}
+
+//====================
+//=== Display Menu ===
+//====================
+void displayMenu(void) {
+
+    std::cout << "\nCommands:\n";
+    std::cout << "C - process a check\n";
+    std::cout << "D - process a deposit\n";    
+    std::cout << "E - end the programn";    
+}
+
+//==============
+//=== Choose ===
+//==============
+void processTransactions(double &balance, double &totalCharges) {
+    char command;
+
+    do {
+        std::cout << "\n\nEnter transaction type: ";
+        std::cin  >> command;
+        command = tolower(command);
+
+        switch(command) {
+            case 'c': processCheck(balance, totalCharges); break;
+            case 'd': processDeposit(balance, totalCharges); break;
+            case 'e': break;
+            default : std::cout << "\nNot a valid command.\n"; break;
+        } 
+    } while (command != 'e');
+}
+
+//=====================
+//=== Display Title ===
+//=====================
+void displayTitle(void) {
+    std::string prgmTitle = "Checkbook Balancing Program"; 
+    std::cout << prgmTitle << std::endl;
+}
+//=========================
+//=== Beginning Balance ===
+//=========================
+double begBalance() {
+    double balance;
+    std::cout << "Enter beginning balance: ";
+    std::cin >> balance;
+    return balance;
 }
