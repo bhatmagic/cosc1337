f0fb75e4cde50d168849d02f77322916516c260d
David Boyd <bhatmagician@gmail.com>
2019-04-09 10:11:10 -0500

    created dailyAvgEaten loop

--- monkeyFood/main.cpp
+++ monkeyFood/main.cpp
@@ -7,11 +7,13 @@
 //=== Globar Vars ===
 //===================
 const int   NUM_MONKEYS  = 3,
-            DAYS_IN_WEEK = 7;
+            DAYS_IN_WEEK = 7,
+            FUNCTIONS    = 3;
 std::string DAYS_NAME[DAYS_IN_WEEK] = { 
             "SUN", "MON", "TUE", "WED",
             "THU", "FRI", "SAT"       };
-float M_FOOD[NUM_MONKEYS][DAYS_IN_WEEK];
+float M_FOOD[NUM_MONKEYS][DAYS_IN_WEEK]    = {0},
+      M_FOOD_CALC[FUNCTIONS][DAYS_IN_WEEK] = {0};
 
 //===========================
 //=== Function Prototypes ===
@@ -25,6 +27,8 @@ int main(void) {
     
     // Declare variables
     float sum      = 0,
+          totSum   = 0,
+          avg      = 0,
           least    = 0,
           greatest = 0;
 
@@ -45,14 +49,16 @@ int main(void) {
     // Calculate data
 
     // getAvgEatenDay
-    for (int i=0; i<NUM_MONKEYS; ++i) { 
-        for (int j=0; j<DAYS_IN_WEEK; ++j) {
-            sum += M_FOOD[i][j];
+    for (int i=0; i<DAYS_IN_WEEK; ++i) { 
+        for (int j=0; j<NUM_MONKEYS; ++j) {
+            M_FOOD_CALC[0][i] += M_FOOD[j][i];
         }
-        std::cout << std::endl;
+        M_FOOD_CALC[0][i] = (M_FOOD_CALC[0][i]/NUM_MONKEYS);
     }
 
-    std::cout << "\nSum = " << sum << std:: endl;
+    std::cout << "Daily Avg of Food Eaten: \n";
+    for (int i=0; i<7; ++i)
+        std::cout << M_FOOD_CALC[0][i] << ",  ";
     
     // getLeastEatenDay
 
@@ -64,7 +70,6 @@ int main(void) {
     for (auto day : DAYS_NAME)
         std::cout << day << " ";
     std::cout << std::endl;
-
 
 
     // End program
