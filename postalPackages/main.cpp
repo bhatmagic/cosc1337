11f73e81fa3a67a025204af37f4d625a20c356d4
dboyd42 <bhatmagician@gmail.com>
2019-05-24 10:41:10 -0500

    added structs and fns

--- postalPackages/main.cpp
+++ postalPackages/main.cpp
@@ -2,7 +2,40 @@
 #include <iostream>
 #include <iomanip>
 
-// struct Package{}
+//==============
+//=== Stucts ===
+//==============
+class Package {
+    public:
+        double wt = 0.0,
+               dim1 = 0.0,
+               dim2 = 0.0,
+               dim3 = 0.0,
+               girth = 0.0,
+               great_dim = 0.0;
+        bool status = 0;
+
+        bool dimStatus() {
+                };
+};
+
+struct Transaction {
+    int count = 0,
+        accept = 0,
+        reject = 0;
+};
+
+//======================
+//=== Fns Prototypes ===
+//======================
+
+void calcPkgData(Package *, Transaction *);
+void display_menu();
+void display_pkgResults(Package *, Transaction *);
+void display_transResults(Transaction *);
+void format_money();
+void getPkgData();
+void run(Package *, Transaction *);
 
 //========================
 //=== Global Variables ===
@@ -13,75 +46,97 @@ double WEIGHT_TO_COST [][14] = {
     {7, 9.90},   {10, 14.95}, {13, 19.40}, {16, 24.20},
     {25, 31.90}, {30, 38.50}, {40, 44.80}, {45, 47.40}, {50, 55.20} }; 
 
+//============
+//=== Main ===
+//============
 int main(void) {
    
     // Format number display
-    std::cout << std::fixed << std::showpoint << std::setprecision(2);
+    format_money();
 
     // Declare variables
-    double package_weight, 
-           d1, d2, d3;  // Package dimensions
-    int transaction_counter = 0,
-        transaction_accepted = 0,
-        transaction_rejected = 0;
-    double shipping_weight;  // will be referenced to WEIGHT_TO_COST[x][]
-    double shipping_cost; // will be referenced to WEIGHT_TO_COST[][x]
-    bool status = 0;
-    double girth,
-           largest_dim;
+    Package pkg;
+    Transaction trans;
 
-    // Display Menu
+    // Run Program
+    display_menu();
+    run(&pkg, &trans);
+    
+    // Display Results
+    display_transResults(&trans);
+
+    return 0;
+}
+
+//============================
+//=== Function Definitions ===
+//============================
+void calcPkgData(Package *p, Transaction *t) {
+    t->count++;
+    if (p->dim1 > p->dim2) { p->great_dim = p->dim1; }
+    else { p->great_dim = p->dim2; }
+    if (p->dim3 > p->great_dim) p->great_dim= p->dim3;
+    p->girth = 2*(p->dim1+p->dim2+p->dim3-p->great_dim);
+
+    // if dim>3ft || girth>5th
+    if (p->dim1 > 48 || p->dim2 > 48 || p->dim3 > 48 || p->girth > 60) {
+        p->status = 0;
+        t->reject++;
+    } else if ( p->status == 1 ) {
+        t->accept++;
+    }
+    
+}
+void display_menu() {
     std::cout << std::endl;
     std::cout << "For each transaction, enter package "
               << "weight and 3 dimensions.\n"
               << "Enter -1 to quit.\n" << std:: endl;
+}
+void display_pkgResults(Package *p, Transaction *t) {
+    std::cout << "Transaction: " << t->count  << std::endl;
+    std::cout << "Status     : " << p->status << std::endl; 
+    std::cout << "Weight     : " << "0.00\n"; //shipping_weight << std::endl;
+    std::cout << "Cost       : " << "0.00\n"; //shipping_cost   << std::endl;
     
+}
+void display_transResults(Transaction *t) {
+    std::cout << "\nNumber of accepted packaged: " << t->accept;
+    std::cout << "\nNumber of rejected packages: " << t->reject;
+    std::cout << std::endl;
+}
+void format_money(){
+    std::cout << std::fixed << std::showpoint << std::setprecision(2);
+}
+void getPkgData(Package *p) { 
+    do { 
+        std::cout << "Enter package weight and 3 dimension: ";
+        std::cin  >> p->wt;
+        if (p->wt == -1) break; 
+        std::cin  >> p->dim1;
+        std::cin  >> p->dim2;
+        std::cin  >> p->dim3;
+        if (p->wt > 50 || p->wt <= 0) { p->status = 0; }
+        else {p->status = 1; }
+        if (p->wt <= 0 || p->dim1 <= 0 || p->dim2 <= 0 || p->dim3 <= 0) {
+            std::cout << "Weight and dimensions must be greater than 0.\n\n";
+        }
+    } while (p->wt <= 0 ||  p->dim1 <= 0 || p->dim2 <= 0 || p->dim3 <= 0);
+    
+}
+void run(Package *pkg, Transaction *trans) {
     do {
         // Get User Data
-        do { // dim checker
-            std::cout << "Enter package weight and 3 dimension: ";
-            std::cin  >> package_weight;
-            if (package_weight == -1) break; 
-            if (package_weight > 50) // if weight>50 pounds
-            status = 0;
-            std::cin  >> d1;
-            std::cin  >> d2;
-            std::cin  >> d3;
-            if (d1 < 0 || d2 < 0 || d3 < 0)
-                std::cout << "Dimensions must be greater than 0.\n\n";
-        } while (d1 < 0 || d2 < 0 || d3 < 0);     // if dim<0 in
+        getPkgData(pkg);
 
         // Calculate Data
-        transaction_counter++;
-        if (d1 > d2) largest_dim = d1;
-        else largest_dim =d2;
-        if (d3 > largest_dim) largest_dim = d3;
-        girth = 2*(d1+d2+d3-largest_dim);
-
-        // if dim>3ft || girth>5th
-        if (d1 > 48 || d2 > 48 || d3 > 48 || girth > 60) {
-            status = 0;
-            transaction_rejected++;
-        } else {
-            status = 1;
-            transaction_accepted++;
-        }
+        calcPkgData(pkg, trans);
 
         // Display Results
-        if (package_weight == -1) {
-            std::cout << std::endl;
-        } else {
-            std::cout << "Transaction: " << transaction_counter << std::endl;
-            std::cout << "Status: " << status << std::endl; 
-            std::cout << "Weight: " << shipping_weight << std::endl;
-            std::cout << "Cost: "   << shipping_cost   << std::endl;
+        std::cout << std::endl;
+        if (pkg->wt != -1) {
+            display_pkgResults(pkg, trans);
         }
-    } while (package_weight != -1);
-
-    // Display EndPrgm Results
-    std::cout << "\nNumber of accepted packaged: " << transaction_accepted;
-    std::cout << "\nNumber of rejected packages: " << transaction_rejected;
-    std::cout << std::endl;
-
-    return 0;
+        std::cout << std::endl;
+    } while (pkg->wt != -1);
 }
