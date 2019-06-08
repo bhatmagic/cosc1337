703b0176642ffc7147c8fe68bf918ff1433038ab
dboyd42 <bhatmagician@gmail.com>
2019-06-08 14:57:49 -0500

    updated vars

--- fizzbuzz/main.cpp
+++ fizzbuzz/main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 
 int main(void) {
+    int min = 1,
+        max = 100;
     
-    for (int i=1; i<101; ++i) {
+    for (int i=min; i<max+1; ++i) {
         if (i%3==0 && i%5==0)
             std::cout << "FizzBuzz\n";
         else if (i%3==0)
